ceb470ab84ee2a4d7b8737d84a632d703abd0ab3
sunitayadav11 <149136190+sunitayadav11@users.noreply.github.com>
2023-10-27 10:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('Hello,World')
-streamlit.header('Breakfast Menu')
+streamlit.header('🥣 🥗 Breakfast Menu  🥑🍞')
 streamlit.text('Omega and blueberry oatmeal')
 streamlit.text('spinach smoothie')
 streamlit.text('boiled eggs')
