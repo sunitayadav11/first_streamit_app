2dc9e5221d9d5fb6344689f09a2f690764fee965
sunitayadav11 <149136190+sunitayadav11@users.noreply.github.com>
2023-10-27 10:56:56 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('Hello,World')
 streamlit.header('Breakfast Menu')
-straemlit.text('Omega and blueberry oatmeal')
-straemlit.text('spinach smoothie')
-straemlit.text('boiled eggs')
+streamlit.text('Omega and blueberry oatmeal')
+streamlit.text('spinach smoothie')
+streamlit.text('boiled eggs')
