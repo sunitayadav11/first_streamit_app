3488759d945e5330d2e6bb811eede024b93c98c6
sunitayadav11 <149136190+sunitayadav11@users.noreply.github.com>
2023-10-27 11:24:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit
 
 streamlit.title('Hello,World')
@@ -5,3 +6,6 @@ streamlit.header('🥣 🥗 Breakfast Menu  🥑🍞')
 streamlit.text('🥣 🥗Omega and blueberry oatmeal')
 streamlit.text('🥑spinach and avacado smoothie')
 streamlit.text('🐔boiled eggs')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
